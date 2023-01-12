d0d352684d30e5f5c92dacf879f9ab705a1c56dc
Fabricio Navarrete <eansteel1995@gmail.com>
2023-01-12 15:19:37 -0500

    feat: add mongo db connection config

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,23 @@
 const app = require('./app')
+const mongoDB = require('./mongodb')
 
 const port = app.get('port') || 5000
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+mongoDB
+  .then((db) => {
+    console.log(
+      `>>> MongoDB connected: ${db.connection.host}/${db.connection.name}`
+    )
+    app.listen(port, () => {
+      console.log(`>>> Server running on port ${port}`)
+    })
+
+    app.on('error', (err) => {
+      console.error(`Error Server: ${err}`)
+      process.exit(1)
+    })
+  })
+  .catch((err) => {
+    console.error(`Error: ${err}`)
+    process.exit(1)
+  })
