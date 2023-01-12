2fe977d5995d62820e032249e5a06e6df7f96236
Fabricio Navarrete <eansteel1995@gmail.com>
2023-01-12 15:21:02 -0500

    feat: add create and get for categories

--- server/src/controller/index.js
+++ server/src/controller/index.js
@@ -1,7 +1,38 @@
-function addCategorie(req, res) {
-  res.json('Get REQUEST from categories')
+const Category = require('../models/category')
+
+async function addCategory(req, res) {
+  try {
+    const { type, color } = req.body
+    const newCategory = await Category.create({
+      type,
+      color,
+    })
+
+    res.json(newCategory)
+  } catch (err) {
+    if (err.message.includes('duplicate key error')) {
+      return res.status(409).json({
+        success: false,
+        error: `Tipo de la categoría duplicado`,
+      })
+    }
+    res.status(500).json(err)
+  }
+}
+
+async function getCategories(_req, res) {
+  try {
+    const data = await Category.find()
+    const filteredData = data.map((v) =>
+      Object.assign({}, { type: v.type, color: v.color })
+    )
+    res.json(filteredData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 }
 
 module.exports = {
-  addCategorie,
+  addCategory,
+  getCategories,
 }
