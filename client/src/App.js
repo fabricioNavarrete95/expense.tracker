b9a2a7d9d1628f67433a2bba9b271ad93493ac9e
Fabricio Navarrete <eansteel1995@gmail.com>
2023-01-19 10:24:18 -0500

    refactor: remove h1 expense app title

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,6 @@ function App() {
   return (
     <div className="App">
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
-        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
-          Control Gastos
-        </h1>
         <div className="grid md:grid-cols-2 gap-4">
           <Graph />
           <Form />
