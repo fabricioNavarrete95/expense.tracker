d801fd56f9f9e8ee3f1b728404c72e6ae21d666e
Fabricio Navarrete <eansteel1995@gmail.com>
2023-01-19 10:09:47 -0500

    add routes for labels and transactions

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -3,9 +3,18 @@ const controller = require('../controller')
 
 const router = Router()
 
+router.get('/api/labels', controller.getLabels)
+
 router
   .route('/api/categories')
   .get(controller.getCategories)
   .post(controller.addCategory)
+
+router
+  .route('/api/transactions')
+  .get(controller.getTransactions)
+  .post(controller.createTransaction)
+
+router.delete('/api/transactions/:id', controller.deleteTransaction)
 
 module.exports = router
